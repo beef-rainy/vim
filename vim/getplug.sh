a4205d8c5f5d7ae164463205351be4978b79abac
Bill Franey <bfraney@sbcglobal.net>
2020-07-21 13:55:12 -0600

    rename vimrc for portability, add getplug.sh

---
+++ vim/getplug.sh
@@ -0,0 +1,6 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+sudo apt-get install universal-ctags
+sudo apt-get install ripgrep
+
+
